165ca70d07a2fe2a23c548e607bd5aa4638f5589
Afua Mensah <38508481+afuamensah@users.noreply.github.com>
2021-08-03 08:07:24 -0500

    Final Web Check for Web Servers

---
+++ finalwc/store.php
@@ -0,0 +1,34 @@
+<?php
+	require("header.php");
+?>
+        <main>
+            <ul id="store">
+                    <li>
+                        <figure>
+                            <a href="1.jpg" data-lightbox="vecta" data-title="Adopt a Cat T-Shirt">
+                            <img src="1.jpg" alt="cat"></a>
+                            <figcaption>Adopt a Cat T-Shirt</figcaption>
+                            <figcaption class="price">$10.49</figcaption>
+                        </figure>
+                    </li>
+                    <li>
+                        <figure>
+                            <a href="2.jpg" data-lightbox="vecta" data-title="Save the Black Rhino T-Shirt">
+                            <img src="2.jpg" alt="rhino"></a>
+                            <figcaption>Save the Black Rhino T-Shirt</figcaption>
+                            <figcaption class="price">$9.99</figcaption>
+                        </figure>
+                    </li>
+                    <li>
+                        <figure>
+                            <a href="3.jpg" data-lightbox="vecta" data-title="Think Before Extinct T-Shirt">
+                            <img src="3.jpg" alt="extinct"></a>
+                            <figcaption>Think Before Extinct T-Shirt</figcaption>
+                            <figcaption class="price">$10.99</figcaption>
+                        </figure>
+                    </li>
+                </ul>
+        </main>
+<?php
+	require("footer.php");
+?>
